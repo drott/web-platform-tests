6dbbfe78e4bf848bccebbca0602295cf0e79a2c8
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-04-22 15:05:44 +0300

    Adding Intel XHR timeout tests.
    Signed-off-by: Dominik Röttsches <dominik.rottsches@intel.com>

---
+++ XMLHttpRequest/resources/delay.php
@@ -0,0 +1,11 @@
+<?php
+  // Code snippet from Opera's XMLHttpRequest test suite.
+
+  // It will always output a text file for now with the literal string
+  // "TEST_DELAY"
+  $delay = ctype_digit($_GET["ms"]) ? $_GET["ms"] : 500;
+  usleep($delay);
+  header("Content-type: text/plain");
+  echo "TEST_DELAY";
+  exit;
+?>
